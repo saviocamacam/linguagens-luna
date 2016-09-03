f25fe98c0e75ccd49b08aee3eba45a7020a720b8
rickftime <rickftime@gmail.com>
2016-09-03 19:03:18 -0300

    Atualização dos arquivos de testes

--- tests/connectionTest.lua
+++ tests/connectionTest.lua
@@ -2,15 +2,17 @@ package.path = package.path .. ';../src/?.lua'
 
 local luaUnit = require('luaunit')
 local conn = require('connection')
+local jsonparser = require('parser')
 
 TestConn = {}
 
 function TestConn:setUp()
-	self.conn = conn
+	self.conn = conn:new()
+	self.parser = jsonparser:new()
 end
 
 	function TestConn:testConnection()
-		result = self.connection.getText(self.conn.getPage('Brasil'))
+		result = self.parser.parse(self.conn.getpage('Brasil'))
 	print(result)
 	luaUnit.assertNotNil(result)
 end
