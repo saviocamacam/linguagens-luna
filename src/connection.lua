3ffd18d0c135c354477043744ed8ed30ac88e785
rickftime <rickftime@gmail.com>
2016-09-04 15:11:35 -0300

    Fix de Bug que não permitia palavra vazia para ativar o random

--- src/connection.lua
+++ src/connection.lua
@@ -4,7 +4,7 @@ local connection = {}
 
 local function getpage(about) 
 	local url = ''
-	if( about ) then
+	if( about and about ~= '' ) then
 		url ='https://pt.wikipedia.org/w/api.php?action=query&format=json&utf8=1&prop=extracts&explaintext=1&exsectionformat=plain&titles=' .. about 
 	else
 		url = 'https://pt.wikipedia.org/w/api.php?action=query&utf8=1&format=json&generator=random&grnnamespace=0&prop=extracts&explaintext=1&exsectionformat=plain'
