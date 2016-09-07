0a8ee71fe5c40ba0b753a0373663c6c819a4f0ab
saviocamacam <savio.camacam@hotmail.com>
2016-09-07 17:55:58 -0300

    Adicionado metodo para selecao de palavras do tamanho especificado pelo usuario

--- src/main.lua
+++ src/main.lua
@@ -4,9 +4,27 @@ local jsonparser = require('parser')
 parser = jsonparser:new()
 conn = connection:new()
 
+function mysplit(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t={} ; i=1
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+		t[i] = str
+    i = i + 1
+	end
+	return t
+end
+
+local function searchWords(content, wordLenght)
+	local tableWords = mysplit(content, " ")
+	print(tableWords)
+end
+
 while menuOption ~= '6' do
 	print('LunaLetroca')
 	print('[1]: Pesquisar novo tema')
+	print('[2]: Informar tamanho da palavra')
 	print('[6]: Sair')
 	menuOption = io.read()
 
@@ -18,5 +36,15 @@ while menuOption ~= '6' do
 		print("Fazendo parsing...")
 		content = parser.parse(json)
 		print(content)
+	end
+
+	if menuOption == '2' then
+		print('Qual o tamanho maximo da palavra?')
+		wordLenght = io.read()
+
+
+		print("Pesquisando palavras...")
+
+		selectWords = searchWords(content, wordLenght)
 	end
 end
