eb7f5b9b582933fa054a25ebabfb53ae77b895e8
saviocamacam <savio.camacam@hotmail.com>
2016-09-12 23:17:18 -0300

    Codigo foi 'traduzido' para facilitar na apresentacao

--- src/main.lua
+++ src/main.lua
@@ -17,31 +17,31 @@ function mysplit(inputstr, sep)
 	return t
 end
 
-local function searchWords(content, wordLenght)
-	local allWords = mysplit(content, "%p+%s")
-	for k, v in pairs(allWords) do
+local function searchWords(conteudo, tamanhoPalavra)
+	local todasPalavras = mysplit(conteudo, "%p+%s")
+	for k, v in pairs(todasPalavras) do
     print(k, v)
   end
 end
 
-local function printTable(table)
-  for k, v in pairs(table) do
+local function imprimeTabela(tabela)
+  for k, v in pairs(tabela) do
     print(k, v)
   end
 end
 
-local function getWordSize(words, size)
-  local wordsInSize = {}
-  for k, v in pairs(words) do
-    if utf8.len(v) == size then
-      table.insert(wordsInSize, v)
+local function pegarPalavrasNoTamanho(palavras, tamanho)
+  local palavrasNoTamanho = {}
+  for k, v in pairs(palavras) do
+    if utf8.len(v) == tamanho then
+      table.insert(palavrasNoTamanho, v)
     end
   end
-  return wordsInSize
+  return palavrasNoTamanho
 end
 
-local function notInTable(selectedWords, wordIn)
-  for k, v in pairs(selectedWords) do
+local function naoNaTabela(palavrasSelecionadas, wordIn)
+  for k, v in pairs(palavrasSelecionadas) do
     if v == wordIn then
       return false
     end
@@ -49,8 +49,8 @@ local function notInTable(selectedWords, wordIn)
   return true
 end
 
-local function inTableIndex(selectedWords, wordIn)
-  for k, v in pairs(selectedWords) do
+local function pegarIndiceTabela(palavrasSelecionadas, wordIn)
+  for k, v in pairs(palavrasSelecionadas) do
     if v == wordIn then
       return k
     end
@@ -58,122 +58,122 @@ local function inTableIndex(selectedWords, wordIn)
   return -1
 end
 
-local function letterInWord(mainWord, char, bool, tableLetters)
+local function letraNaPalavra(palavraPrincipal, char, bool, tabelaLetras)
   if bool then
-    printTable(tableLetters)
+    imprimeTabela(tabelaLetras)
   end
 
-  for c in newUtf8.gmatch(mainWord, ".") do
-    if c == char and tableLetters[c] > 0 then
-      tableLetters[c] = tableLetters[c] - 1
+  for c in newUtf8.gmatch(palavraPrincipal, ".") do
+    if c == char and tabelaLetras[c] > 0 then
+      tabelaLetras[c] = tabelaLetras[c] - 1
       return true
     end
   end
   return false
 end
 
-local function selectWords(allWords, wordLenght)
-  local wordsLengthN = getWordSize(allWords, wordLenght)
-  local position = math.random(#wordsLengthN)
-  local mainWord = wordsLengthN[position]
-  local wordsInZeroToWordLenght = {}
-  local selectedWords = {}
+local function selectWords(todasPalavras, tamanhoPalavra)
+  local palavrasTamanhoN = pegarPalavrasNoTamanho(todasPalavras, tamanhoPalavra)
+  local posicao = math.random(#palavrasTamanhoN)
+  local palavraPrincipal = palavrasTamanhoN[posicao]
+  local palavrasMinimoMaximo = {}
+  local palavrasSelecionadas = {}
 
-  for k, v in pairs(allWords) do
-    if utf8.len(v) <= wordLenght then
-      table.insert(wordsInZeroToWordLenght, v)
+  for k, v in pairs(todasPalavras) do
+    if utf8.len(v) <= tamanhoPalavra then
+      table.insert(palavrasMinimoMaximo, v)
     end
   end
 
-  local counter = 0
-  for k, v in pairs(wordsInZeroToWordLenght) do
+  local contador = 0
+  for k, v in pairs(palavrasMinimoMaximo) do
 
-    local tableLetters = {}
-    for c in newUtf8.gmatch(mainWord, ".") do
-      if not tableLetters[c] then
-        tableLetters[c] = 1
+    local tabelaLetras = {}
+    for c in newUtf8.gmatch(palavraPrincipal, ".") do
+      if not tabelaLetras[c] then
+        tabelaLetras[c] = 1
       else
-        tableLetters[c] = tableLetters[c] + 1
+        tabelaLetras[c] = tabelaLetras[c] + 1
       end
     end
 
     for c in newUtf8.gmatch(v, ".") do
 
-      local boolean = letterInWord(mainWord, c, bool, tableLetters)
+      local boolean = letraNaPalavra(palavraPrincipal, c, bool, tabelaLetras)
       if boolean then
-        counter = counter + 1
+        contador = contador + 1
       end
     end
 
-    if counter == utf8.len(v) and notInTable(selectedWords, v) then
-      table.insert(selectedWords, v)
+    if contador == utf8.len(v) and naoNaTabela(palavrasSelecionadas, v) then
+      table.insert(palavrasSelecionadas, v)
     end
-    counter = 0
+    contador = 0
   end
 
-  --printTable(selectedWords)
-  print("Principal: ", position, mainWord)
-  return selectedWords, mainWord
+  --imprimeTabela(palavrasSelecionadas)
+  print("Principal: ", posicao, palavraPrincipal)
+  return palavrasSelecionadas, palavraPrincipal
 end
 
-function createMask(selectedWords)
-  local wordMask = {}
+function criarMascara(palavrasSelecionadas)
+  local mascarasPalavras = {}
   local a = ""
-  local biggest = ""
-  local biggestTable = {}
-  for k, v in pairs(selectedWords) do
+  local maiorMascara = ""
+  local maiorMascaraTabela = {}
+  for k, v in pairs(palavrasSelecionadas) do
     for c in newUtf8.gmatch(v, ".") do
       a = a .."_ "
     end
-    if utf8.len(a) > utf8.len(biggest) then
-      biggest = a
-      biggestTable = {}
+    if utf8.len(a) > utf8.len(maiorMascara) then
+      maiorMascara = a
+      maiorMascaraTabela = {}
       for c in newUtf8.gmatch(a, ".") do
-        table.insert(biggestTable, c)
+        table.insert(maiorMascaraTabela, c)
       end
     end
-    table.insert(wordMask, a)
+    table.insert(mascarasPalavras, a)
     a = ""
   end
-  return wordMask, biggestTable
+  return mascarasPalavras, maiorMascaraTabela
 end
 
-function shuffleWord(mainWord)
-  local mainWordTable = {}
-  local squigglyWordTable = {}
-  local tablePositions = {}
-  for c in newUtf8.gmatch(mainWord, ".") do
-    table.insert(mainWordTable, c)
+function embaralhaPalavra(palavraPrincipal)
+  local palavraPrincipalTabela = {}
+  local palavrasEmbaralhadasTabela = {}
+  local tabelaPosicoes = {}
+  for c in newUtf8.gmatch(palavraPrincipal, ".") do
+    table.insert(palavraPrincipalTabela, c)
   end
 
   local i = 1
-  while i ~= #mainWordTable+1 do
-    local position = math.random(#mainWordTable)
-    local letter = mainWordTable[position]
-    if notInTable(tablePositions, position) then
-      table.insert(tablePositions, position)
-      table.insert(squigglyWordTable, letter)
+  while i ~= #palavraPrincipalTabela+1 do
+    local posicao = math.random(#palavraPrincipalTabela)
+    local letra = palavraPrincipalTabela[posicao]
+    if naoNaTabela(tabelaPosicoes, posicao) then
+      table.insert(tabelaPosicoes, posicao)
+      table.insert(palavrasEmbaralhadasTabela, letra)
       i = i + 1
     end
   end
 
-  return squigglyWordTable
+  return palavrasEmbaralhadasTabela
 end
 
-function tableToString(squigglyWordTable, bool)
-  local squigglyWord = ""
-  for k, v in pairs(squigglyWordTable) do
+function tabelaParaString(palavrasEmbaralhadasTabela, bool)
+  local palavraEmbaralhada = ""
+  for k, v in pairs(palavrasEmbaralhadasTabela) do
     if bool then
-      squigglyWord = squigglyWord .. v .." "
+      palavraEmbaralhada = palavraEmbaralhada .. v .." "
     end
     if v ~= '_' and v ~= ' ' and not bool then
-      squigglyWord = squigglyWord .. v
+      palavraEmbaralhada = palavraEmbaralhada .. v
     end
   end
-  return squigglyWord
+  return palavraEmbaralhada
 end
 
-function showMenu()
+function exibirMenu()
   print('LunaLetroca')
 	print('[1]: Pesquisar novo tema')
 	print('[2]: Informar tamanho da palavra')
@@ -181,159 +181,172 @@ function showMenu()
 	print('[6]: Sair')
 end
 
-function printMask(wordMask, squigglyWord, biggestMask)
-  printTable(wordMask)
-  print("\nSquiggly Word: ", squigglyWord)
-  print("\nMascara: ", biggestMask)
+function imprimeMascara(mascarasPalavras, palavraEmbaralhada, maiorMascara)
+  imprimeTabela(mascarasPalavras)
+  print("\nSquiggly Word: ", palavraEmbaralhada)
+  print("\nMascara: ", maiorMascara)
 end
 
-function insertLetter(biggestMaskTable, newLetter)
-  for k, v in pairs(biggestMaskTable) do
+function insereLetra(maiorMascaraTabela, novaLetra)
+  for k, v in pairs(maiorMascaraTabela) do
     if v == '_' then
-      biggestMaskTable[k] = newLetter
+      maiorMascaraTabela[k] = novaLetra
       return
     end
   end
 end
 
-function removeLetter(squigglyWordTable, newLetter)
-  for k, v in pairs(squigglyWordTable) do
-    if v == newLetter then
-      table.remove(squigglyWordTable, k)
+function removeLetra(palavrasEmbaralhadasTabela, novaLetra)
+  for k, v in pairs(palavrasEmbaralhadasTabela) do
+    if v == novaLetra then
+      table.remove(palavrasEmbaralhadasTabela, k)
       return
     end
   end
 end
 
-function resetBiggestMask(wordMaskTable)
-  for k, v in pairs(wordMaskTable) do
+function resetMaiorMascara(mascaraPalavraTabela)
+  for k, v in pairs(mascaraPalavraTabela) do
     if v ~= ' ' and v ~= '_' then
-      wordMaskTable[k] = '_'
+      mascaraPalavraTabela[k] = '_'
     end
   end
 end
 
-function pop(wordMaskTable)
-  for k, v in pairs(wordMaskTable) do
+function pop(mascaraPalavraTabela)
+  for k, v in pairs(mascaraPalavraTabela) do
     if v == '_' and k > 2 then
-      local letter = wordMaskTable[k-2]
-      wordMaskTable[k-2] = '_'
-      return letter
+      local letra = mascaraPalavraTabela[k-2]
+      mascaraPalavraTabela[k-2] = '_'
+      return letra
     end
-    if k == #wordMaskTable then
-      local letter = wordMaskTable[k-1]
-      wordMaskTable[k-1] = '_'
-      return letter
+    if k == #mascaraPalavraTabela then
+      local letra = mascaraPalavraTabela[k-1]
+      mascaraPalavraTabela[k-1] = '_'
+      return letra
     end
   end
 end
 
-function pull(squigglyWordTable, letter)
-  table.insert(squigglyWordTable, letter)
+function pull(palavrasEmbaralhadasTabela, letra)
+  table.insert(palavrasEmbaralhadasTabela, letra)
 end
 
-showMenu()
-menuOption = io.read()
+--exibirMenu()
+--print("Pesquise um tema: ")
+opcaoMenu = '1'
 
-while menuOption ~= '6' do
-
-	if menuOption == '1' then
+while opcaoMenu ~= '6' do
+	if opcaoMenu == '1' then
 		io.write("Digite um tema a ser pesquisado: ")
 		tema = string.format("%s", io.read())
 		print("Fazendo a requisição no Wikipedia...")
 		json = conn.getpage(tema)
 		print("Fazendo parsing...")
-		content = parser.parse(json)
-		--print(content)
-    if content ~= nil then
-      allWords = mysplit(content, " ,.\n")
+		conteudo = parser.parse(json)
+		print("\n", conteudo)
+
+    if conteudo == "" then
+      print("Nenhum resultado para essa pesquisa!")
     end
+
+    if conteudo ~= nil then
+      todasPalavras = mysplit(conteudo, " ,.\n")
+    end
+
   end
 
-	if menuOption == '2' then
-		print('Qual o tamanho maximo da palavra?')
-		wordLenght = io.read("*number")
-		print("Pesquisando palavras...")
+	if opcaoMenu == '2' then
+    if conteudo == "" then
+      print("Nenhum resultado para essa pesquisa!")
+    else
 
-    selectedWords, mainWord = selectWords(allWords, wordLenght)
-    printTable(selectedWords)
-    wordMask, biggestMaskTable = createMask(selectedWords)
+      print('Qual o tamanho maximo da palavra?')
+  		tamanhoPalavra = io.read("*number")
+  		print("Pesquisando palavras...")
 
-    squigglyWordTable = shuffleWord(mainWord)
-    squigglyWord = tableToString(squigglyWordTable, true)
+      palavrasSelecionadas, palavraPrincipal = selectWords(todasPalavras, tamanhoPalavra)
+      imprimeTabela(palavrasSelecionadas)
+      mascarasPalavras, maiorMascaraTabela = criarMascara(palavrasSelecionadas)
 
-    --menuOption = '3'
+      palavrasEmbaralhadasTabela = embaralhaPalavra(palavraPrincipal)
+      palavraEmbaralhada = tabelaParaString(palavrasEmbaralhadasTabela, true)
+    end
 	end
 
-  if menuOption == '3' then
+  if opcaoMenu == '3' then
 
-    squigglyWordTable = shuffleWord(mainWord)
-    squigglyWord = tableToString(squigglyWordTable, true)
-    biggestMask = tableToString(biggestMaskTable, true)
-    printMask(wordMask, squigglyWord, biggestMask)
-    io.write("Nova entrada: ")
-    local newLetter = io.read()
-
-    while newLetter ~= '1' and newLetter ~= '2' and newLetter ~= '3' do
-      if not notInTable(squigglyWordTable, newLetter) then
-        print("tem: ", newLetter)
-
-        insertLetter(biggestMaskTable, newLetter)
-        removeLetter(squigglyWordTable, newLetter)
-        squigglyWord = tableToString(squigglyWordTable, true)
-        biggestMask = tableToString(biggestMaskTable, true)
-        printMask(wordMask, squigglyWord, biggestMask)
-
-      else
-        print("nao tem: ", newLetter)
-      end
-      io.write("Testar? >> ! : ")
+    if conteudo == "" then
+      print("Nenhum resultado para essa pesquisa!")
+    else
+      palavrasEmbaralhadasTabela = embaralhaPalavra(palavraPrincipal)
+      palavraEmbaralhada = tabelaParaString(palavrasEmbaralhadasTabela, true)
+      maiorMascara = tabelaParaString(maiorMascaraTabela, true)
+      imprimeMascara(mascarasPalavras, palavraEmbaralhada, maiorMascara)
       io.write("Nova entrada: ")
-      newLetter = io.read()
+      local novaLetra = io.read()
 
-      if newLetter == '!' then
-        wordForTest = tableToString(biggestMaskTable, false)
-        print("For test: ", wordForTest)
-        indexOfWord = inTableIndex(selectedWords, wordForTest)
+      while novaLetra ~= '1' and novaLetra ~= '2' and novaLetra ~= '3' do
+        if not naoNaTabela(palavrasEmbaralhadasTabela, novaLetra) then
+          print("tem: ", novaLetra)
 
-        if indexOfWord > 0 then
-          print("Essa palavra existe!")
-          wordMask[indexOfWord] = wordForTest
-          resetBiggestMask(biggestMaskTable)
-
-          squigglyWordTable = shuffleWord(mainWord)
-          squigglyWord = tableToString(squigglyWordTable, true)
-          biggestMask = tableToString(biggestMaskTable, true)
-          printMask(wordMask, squigglyWord, biggestMask)
-          io.write("Nova entrada: ")
-          newLetter = io.read()
+          insereLetra(maiorMascaraTabela, novaLetra)
+          removeLetra(palavrasEmbaralhadasTabela, novaLetra)
+          palavraEmbaralhada = tabelaParaString(palavrasEmbaralhadasTabela, true)
+          maiorMascara = tabelaParaString(maiorMascaraTabela, true)
+          imprimeMascara(mascarasPalavras, palavraEmbaralhada, maiorMascara)
 
         else
-          print("Essa palavra nao existe aqui!")
-          printMask(wordMask, squigglyWord, biggestMask)
-          io.write("Nova entrada: ")
-          newLetter = io.read()
+          print("nao tem: ", novaLetra)
         end
-      end
-
-      if newLetter == '*' then
-        letterP = pop(biggestMaskTable)
-        print("l: ", letterP)
-        pull(squigglyWordTable, letterP)
-        squigglyWord = tableToString(squigglyWordTable, true)
-        biggestMask = tableToString(biggestMaskTable, true)
-        printMask(wordMask, squigglyWord, biggestMask)
-
+        io.write("Testar? >> ! : ")
         io.write("Nova entrada: ")
-        newLetter = io.read()
+        novaLetra = io.read()
+
+        if novaLetra == '!' then
+          PalavraTeste = tabelaParaString(maiorMascaraTabela, false)
+          print("Para teste: ", PalavraTeste)
+          indicePalavra = pegarIndiceTabela(palavrasSelecionadas, PalavraTeste)
+
+          if indicePalavra > 0 then
+            print("Essa palavra existe!")
+            mascarasPalavras[indicePalavra] = PalavraTeste
+            resetMaiorMascara(maiorMascaraTabela)
+
+            palavrasEmbaralhadasTabela = embaralhaPalavra(palavraPrincipal)
+            palavraEmbaralhada = tabelaParaString(palavrasEmbaralhadasTabela, true)
+            maiorMascara = tabelaParaString(maiorMascaraTabela, true)
+            imprimeMascara(mascarasPalavras, palavraEmbaralhada, maiorMascara)
+            io.write("Nova entrada: ")
+            novaLetra = io.read()
+
+          else
+            print("Essa palavra nao existe aqui!")
+            imprimeMascara(mascarasPalavras, palavraEmbaralhada, maiorMascara)
+            io.write("Nova entrada: ")
+            novaLetra = io.read()
+          end
+        end
+
+        if novaLetra == '*' then
+          letraRemovida = pop(maiorMascaraTabela)
+          print("l: ", letraRemovida)
+          pull(palavrasEmbaralhadasTabela, letraRemovida)
+          palavraEmbaralhada = tabelaParaString(palavrasEmbaralhadasTabela, true)
+          maiorMascara = tabelaParaString(maiorMascaraTabela, true)
+          imprimeMascara(mascarasPalavras, palavraEmbaralhada, maiorMascara)
+
+          io.write("Nova entrada: ")
+          novaLetra = io.read()
+        end
       end
     end
   end
 
-  if menuOption ~= '3' then
-    showMenu()
-    menuOption = io.read()
+  if opcaoMenu ~= '3' then
+    exibirMenu()
+    opcaoMenu = io.read()
   else
-    menuOption = newLetter
+    opcaoMenu = novaLetra
   end
 end
